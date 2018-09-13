b4c36a7f22e405e95bd74493ccbcf70a6588c590
Gustav <gustav@invoke.se>
2018-09-13 10:38:57 +0200

    Fix endpoints

--- lib/bing_bong/endpoint.rb
+++ lib/bing_bong/endpoint.rb
@@ -2,12 +2,12 @@ class Endpoint
 
   ENDPOINTS = {
     customer_management_service: {
-      production: "https://clientcenter.api.bingads.microsoft.com/Api/CustomerManagement/v12/CustomerManagementService.svc?wsdl",
-      sandbox: "https://clientcenter.api.sandbox.bingads.microsoft.com/Api/CustomerManagement/v12/CustomerManagementService.svc?wsdl"
+      production: "https://clientcenter.api.bingads.microsoft.com/Api/CustomerManagement/v12/CustomerManagementService.svc?singleWsdl",
+      sandbox: "https://clientcenter.api.sandbox.bingads.microsoft.com/Api/CustomerManagement/v12/CustomerManagementService.svc?singleWsdl"
     },
     campaign_management_service: {
-      production: 'https://campaign.api.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v12/CampaignManagementService.svc?wsdl',
-      sandbox: 'https://campaign.api.sandbox.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v12/CampaignManagementService.svc?wsdl'
+      production: 'https://campaign.api.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v12/CampaignManagementService.svc?singleWsdl',
+      sandbox: 'https://campaign.api.sandbox.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v12/CampaignManagementService.svc?singleWsdl'
     }
   }
 
